6d4e62e54aea4ee1d1de14c49190537aa61358ec
DiamondGotCat <124330624+DiamondGotCat@users.noreply.github.com>
2024-07-03 15:49:02 +0900

    Add files via upload

---
+++ Runners/template_runner.bundle/runner.js
@@ -0,0 +1,13 @@
+// runner.js
+function run() {
+    console.log("Template Runner is running");
+    // Runnerのメインロジックをここに追加
+}
+
+// 必要に応じて、他の関数を定義
+function exampleFunction() {
+    console.log("Example function");
+}
+
+// メインの実行関数をエクスポート
+module.exports = { run, exampleFunction };
